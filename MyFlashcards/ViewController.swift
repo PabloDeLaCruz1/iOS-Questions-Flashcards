6f2207af572b29dd7e9a89a0e7160a377f49340e
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-03-01 11:56:41 -0500

    Initial Commit

---
+++ MyFlashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFlashcards
+//
+//  Created by Pablo De La Cruz on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
